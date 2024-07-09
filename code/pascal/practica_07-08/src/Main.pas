5ff348281d6e6c1211d04d488b9cf8d3b341f887
Emilio Devesa <emilio@ubuntu-vm>
2024-07-09 12:12:41 +0200

    [pascal/practica_07-08] Added Main.pas

---
+++ code/pascal/practica_07-08/src/Main.pas
@@ -0,0 +1,15 @@
+program Main;
+{   Práctica 2007-2008
+    Command Line program written in Pascal ISO 10206 (Extended Pascal).
+    More info: README.md
+
+    Emilio Devesa
+    https://emiliodevesa.wordpress.com/
+}
+
+import  StandardInput; StandardOutput;
+
+
+begin
+    
+end.
